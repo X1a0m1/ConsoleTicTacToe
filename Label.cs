cb7d80214ba36dd56bf831c88790eecb75e4df7e
X1a0m1 <deniscalko200508@gmail.com>
2023-07-26 14:27:32 +0300

    Fixed #1 & completed Label

---
+++ Label.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToe;
+
+internal class Label : TuiElement
+{
+    public string Text { get; set; } = string.Empty;
+    public ConsoleColor Color { get; set; } = ConsoleColor.Gray;
+    public override void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.Write(Text);
+        Console.ResetColor();
+    }
+}
